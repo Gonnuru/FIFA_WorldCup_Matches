eb49149c7a7695d9341cd0360d9186dd3d5eb70d
Sampath <sampath.gonnuru@gmail.com>
2020-05-26 04:38:06 -0400

    Code for Visualization

---
+++ script.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv('WorldCupMatches.csv')
+print(df.head())
+
+df['Total Goals'] = df['Home Team Goals'] + df['Away Team Goals']
+print(df['Total Goals'].head())
+
+sns.set_style('whitegrid')
+sns.set_context('poster', font_scale = 0.8)
+
+f, ax = plt.subplots(figsize = (12, 7))
+ax = sns.barplot(x = "Year",
+                 y = "Total Goals",
+                 data = df)
+ax.set_title("Average Number of Goals Scored In Word Cup Matches By Year")   
+
+
+
+
+df_goals = pd.read_csv('goals.csv')
+print(df_goals.head())
+
+sns.set_style('whitegrid')
+sns.set_context('notebook', font_scale = 1.25)
+f, ax2 = plt.subplots(figsize = (12, 7))
+
+ax2 = sns.boxplot(x = "year",
+                  y = "goals",
+                  data = df_goals,
+                  palette = "Spectral")
+ax2.set_title("Goals Visualization")
+
+plt.show()
+                 
+
+
+
+
